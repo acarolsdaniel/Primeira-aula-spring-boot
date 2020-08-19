47f3b4affa77ed63a841955a10252d748ec009c4
Ana Carol <karol.internet@hotmail.com>
2020-08-19 10:40:04 -0300

    projeto criado

---
+++ src/main/java/com/exemplo/primeiraAula/PrimeiraAulaApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.primeiraAula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiraAulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiraAulaApplication.class, args);
+	}
+
+}
