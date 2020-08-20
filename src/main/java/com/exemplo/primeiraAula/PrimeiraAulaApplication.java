d06ec94154647436e9b3666cd226df13cbefb138
Ana Carol <karol.internet@hotmail.com>
2020-08-19 22:20:20 -0300

    operação de instanciação

--- src/main/java/com/exemplo/primeiraAula/PrimeiraAulaApplication.java
+++ src/main/java/com/exemplo/primeiraAula/PrimeiraAulaApplication.java
@@ -1,13 +1,34 @@
 package com.exemplo.primeiraAula;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.exemplo.primeiraAula.domain.Categoria;
+import com.exemplo.primeiraAula.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class PrimeiraAulaApplication {
+public class PrimeiraAulaApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeiraAulaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+
 	}
 
 }
